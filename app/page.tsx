17ea04c9271f4770dc2028dd9f52fff1786314ae
syedalirazazaidi <aaliraza007@hotmail.com>
2024-07-30 23:47:48 +0500

    push image to the docker hub

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hi nextjs developer this program for test dockers
+      hi nextjs developer this program for test dockers ... how do d oyou
     </main>
   );
 }
